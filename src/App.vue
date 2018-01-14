a9479f10df9b6f58b2bceb5b6677317b84f38bca
$cott <scott.queen@bixal.com>
2018-01-13 22:45:17 -0500

    New Neo-Noir Theme option

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <router-view/>
-    <footer>Front End by G. Scott Queen.</footer>
+    <footer>
+      <p>Front End by G. Scott Queen.</p>
+      <button @click="darkTheme()">Neo-Noir</button>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  methods: {
+    darkTheme: function darkTheme() {
+      document.body.classList.toggle('neo-noir');
+    },
+  },
 };
 </script>
 
@@ -19,13 +27,25 @@ export default {
   color: #ffffff;
 }
 
-body {
+body.ultra {
   background-color: #646AF4;
+}
+
+body.neo-noir {
+  background-color: #1E1D2E;
 }
 
 footer {
   padding: 5% 10%;
   position: absolute;
   bottom: 0;
+}
+
+button {
+  border: solid #fff 1px;
+  padding: 5px 10px;
+  background: none;
+  color: white;
+  cursor: pointer;
 }
 </style>
