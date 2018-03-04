9147bc3d88ecd4343b483f8b67ab5e9157a699a7
$cott <scott.queen@bixal.com>
2018-03-04 16:53:53 -0500

    new menu icon, gradient on ultra theme

--- src/components/03-organisims/Navigator.vue
+++ src/components/03-organisims/Navigator.vue
@@ -2,7 +2,7 @@
 <div class="main-nav">
   <i
   tabindex="0"
-  class="main-nav--icon el-icon-more"
+  class="main-nav--icon"
   v-on:click="toggleMenu"
   v-on:keyup="toggleMenu"></i>
   <navigation></navigation>
@@ -47,9 +47,13 @@ export default {
     top: 10px;
     right: 10px;
     font-size: 40px;
-    color: #ffe7c3;
     cursor: pointer;
     z-index: 9999;
+    background-image: url('../../assets/if_essential_set_menu_1110965.svg');
+    background-repeat: no-repeat;
+    background-size: contain;
+    height: 35px;
+    width: 35px;
   }
 
   .main-nav--items {
@@ -63,11 +67,31 @@ export default {
       width: 100vw;
       height: 100vh;
       margin: 0;
-      background: #ecf5eb;
+      background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+      background-size: 400% 400%;
+      -webkit-animation: Gradient 20s ease infinite;
+      -moz-animation: Gradient 20s ease infinite;
+      animation: Gradient 20s ease infinite;
       list-style: none;
 
+      /*our animation frames*/
+      @keyframes Gradient {
+        0% {
+          background-position: 0% 50%
+        }
+
+        50% {
+          background-position: 100% 50%
+        }
+
+        100% {
+          background-position: 0% 50%
+        }
+
+      }
+
       a {
-        color: #ffe7c3;
+        color: #ffffff;
         font-size: 55px;
         line-height: 1.2;
         font-weight: bold;
