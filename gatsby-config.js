e824d71bd3b657bad3d8837875ebc9728093db84
gscottqueen <scott.queen@bixal.com>
2022-01-14 10:36:05 -0500

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `G. Scott Queen | Art and Creative Code`,
+    description: `Conceptual artist combining technology and design for one of a kind installations in unique spaces.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import "./layout.css";
 
 const Layout = ({ children, bgImage = null, nonav }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
 
   const ImageURL =
     bgImage !== null ? bgImage.childImageSharp.fluid.src : bgImage;
