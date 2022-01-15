63ca631c0af94b4a19888e8dc32bfa6fe5afb049
gscottqueen <scott.queen@bixal.com>
2022-01-15 13:03:42 -0500

    update SEO

--- src/templates/article-template.js
+++ src/templates/article-template.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useMemo } from "react";
 
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import "./article-template.css";
 
-const ArticleTemplate = ({ title, children }) => (
+const ArticleTemplate = ({ path, children }) => {
+    const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        edges {
+          node {
+            slug
+            frontmatter {
+              title
+              year
+              image
+              description
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const findFrontMatter = useMemo(
+    () =>
+      data.allMdx.edges.find(
+        ({ node }) => node.slug === path.replace(/^\/|\/$/g, "")
+      ),
+    [data, path]
+  );
+
+  if (!findFrontMatter) return null;
+  let { node: { frontmatter } = {} } = findFrontMatter;
+  const { title, description } = frontmatter;
+
+
+  return (
   <Layout nonav>
-    <SEO title={`G. Scott Queen | ${title}`} />
+    <SEO title={title} description={description}/>
     <article id={`${title}`}>{children}</article>
   </Layout>
-);
+)};
 
 export default ArticleTemplate;
