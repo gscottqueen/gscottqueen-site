66931be7ea0dea595a55862f28aade1f4ddd72d1
gscottqueen <scott.queen@bixal.com>
2022-01-17 15:02:52 -0500

    updated SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,14 +23,6 @@ function SEO({ description, lang, meta, title, defaultImage }) {
   const defaultTitle = title !== undefined ? `G. Scott Queen | ${title}` : site.siteMetadata?.title;
   const defaultMetaImage = defaultImage !== undefined ? `${site.siteMetadata.baseUrl}${defaultImage}` : `${site.siteMetadata.baseUrl}${site.siteMetadata.defaultImage}`
 
-
-  <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@nytimes">
-<meta name="twitter:creator" content="@SarahMaslinNir">
-<meta name="twitter:title" content="Parade of Fans for Houston’s Funeral">
-<meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.">
-<meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"></meta>
-
   return (
     <Helmet
       htmlAttributes={{
@@ -63,16 +55,20 @@ function SEO({ description, lang, meta, title, defaultImage }) {
           content: title,
         },
         {
-          property: `og:description`,
-          content: metaDescription,
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `og:url`,
+          content: site.siteMetadata.baseUrl
         },
         {
           property: `og:image`,
           content: defaultMetaImage,
         },
         {
-          property: `og:type`,
-          content: `website`,
+          property: `og:description`,
+          content: metaDescription,
         },
         {
           name: `twitter:card`,
