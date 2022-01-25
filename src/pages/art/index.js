9a1ab9202c06615b3ddeaf23d1747ddee458bb36
gscottqueen <scott.queen@bixal.com>
2022-01-24 20:39:49 -0500

    updated listing component in art works index

--- src/pages/art/index.js
+++ src/pages/art/index.js
@@ -1,43 +1,57 @@
 import React from "react";
 
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../../components/layout";
 import SEO from "../../components/seo";
+// import Image from "../../components/image";
 import ListingTemplate from "../../templates/listing-template";
 import { Link } from "gatsby";
+
+const ArtListing = ({groupValue}) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            year
+            medium
+            image
+            alt
+            group
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+    return (
+      <ul>
+        <h2>{groupValue}</h2>
+        {data.allMdx.nodes.map((item,i) => {
+          const { group, slug, title, year } = item.frontmatter
+          console.log(group, groupValue)
+          return (
+            group === groupValue &&
+            <li key={`${group}-item-${i}`}>
+              <Link to={`/art/${slug}/`}>
+                {title}, {year}
+              </Link>
+            </li>
+          )}
+        )}
+    </ul>
+  )
+}
 
 const IndexArt = () => (
   <Layout>
     <SEO title="Art" />
     <ListingTemplate title="Art">
       <ul>
-        <ul>
-          <h2>Sculpture</h2>
-          <li>
-            <Link to="/art/untitled-sculpture-1">
-              Untitled Sculpture #1, 2011
-            </Link>
-          </li>
-          <li>
-            <Link to="/art/persistence-of-man-over-nature">
-              Persistence of Man Over Nature, 2010
-            </Link>
-          </li>
-        </ul>
-        <ul>
-          <h2>Painting</h2>
-          <li>
-            <Link to="/art/on-the-way-to-telos">On the Way to Telos, 2012</Link>
-          </li>
-          <li>
-            <Link to="/art/we-are-one">We Are One, 2012</Link>
-          </li>
-          <li>
-            <Link to="/art/dark-matter">Dark Matter, 2011</Link>
-          </li>
-          <li>
-            <Link to="/art/strange-days">Strange Days, 2012</Link>
-          </li>
-        </ul>
+        <ArtListing groupValue="Sculpture" />
+        <ArtListing groupValue="Painting" />
       </ul>
     </ListingTemplate>
   </Layout>
