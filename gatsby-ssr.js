19dc6b3b43e4b94dce18a837dec5cdf5df3e44a6
gscottqueen <geoffreysqueen@gmail.com>
2022-03-18 13:14:38 -0400

    trying another appraoch in ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,17 +9,16 @@
 
 // Hack, to reorder the helmet components as first in <head> tag
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-    /**
-     * @type {any[]} headComponents
-     */
-    const headComponents = getHeadComponents();
+  /**
+   * @type {any[]} headComponents
+   */
+  const headComponents = getHeadComponents()
 
-    headComponents.sort((a, b) => {
-    console.log(a)
-        if (a.props && a.props["data-react-helmet"]) {
-            return 0;
-        }
-        return 1;
-    });
-    replaceHeadComponents(headComponents);
-};
+  headComponents.sort((a, b) => {
+    if (a.props && a.props['data-react-helmet']) {
+      return 0
+    }
+    return 1
+  })
+  replaceHeadComponents(headComponents)
+}
