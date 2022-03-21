d458c6fff82a0a89c336fe95d6fb576f15914365
gscottqueen <geoffreysqueen@gmail.com>
2022-03-20 22:12:47 -0400

    remove unused code

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,20 +3,3 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-
-exports.createSchemaCustomization = ({ actions: { createTypes } }) => {
-  createTypes(`
-    type Mdx implements Node {
-      frontmatter: MdxFrontmatter
-    }
-
-    type MdxFrontmatter {
-      items: [ItemValues]
-      content: String @mdx
-    }
-
-    type ItemValues {
-      value: String @mdx
-    }
-  `)
-}
