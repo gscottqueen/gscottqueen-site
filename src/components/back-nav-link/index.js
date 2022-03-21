01185f10366c0d6de7c6ae27ab4a7c519b6d80e7
gscottqueen <geoffreysqueen@gmail.com>
2022-03-21 17:53:20 -0400

    clean up format

--- src/components/back-nav-link/index.js
+++ src/components/back-nav-link/index.js
@@ -1,26 +1,28 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 
 import './index.css'
 
 const BackNavLink = ({ location }) => {
-  const refLocation = window?.refLocation === undefined
-    ? `/${location}`
-      :`${window?.refLocation?.pathname}`
+  const refLocation =
+    window?.refLocation === undefined
+      ? `/${location}`
+      : `${window?.refLocation?.pathname}`
 
-  const linkTitle = window?.refLocation === undefined
-    ? `⇠ Back to ${location}`
-      : `x Close`
+  const linkTitle =
+    window?.refLocation === undefined ? `⇠ Back to ${location}` : `x Close`
 
-return (
-  <div className="back-nav-link--wrapper">
-    <Link
-      to={refLocation}
-      className="back-nav-link"
-      onClick={() => window.refLocation = undefined}>
-      {linkTitle}
-    </Link>
-  </div>
-)}
+  return (
+    <div className="back-nav-link--wrapper">
+      <Link
+        to={refLocation}
+        className="back-nav-link"
+        onClick={() => (window.refLocation = undefined)}
+      >
+        {linkTitle}
+      </Link>
+    </div>
+  )
+}
 
 export default BackNavLink
