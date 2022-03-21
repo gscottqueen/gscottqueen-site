9fe27e8c0a34f378a0e16d7b17556f07845a3d98
gscottqueen <geoffreysqueen@gmail.com>
2022-03-21 10:46:47 -0400

    update github footer format

--- src/components/github-footer/index.js
+++ src/components/github-footer/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
-const GithubFooter = ({ slug }) =>
-  <><hr /><p className="github-footer">All writings are open for peer review and contributions. Visit <a target="_blank" rel="noreferrer" href={`https://github.com/gscottqueen/gscottqueen-site/edit/main/src/content/writing/${slug}.mdx`}>the remote origin</a> to propose changes in a version controlled env.</p></>
+const GithubFooter = ({ slug }) => (
+  <>
+    <hr />
+    <p className="github-footer">
+      All writings are open for peer review and contributions. Visit{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://github.com/gscottqueen/gscottqueen-site/edit/main/src/content/writing/${slug}.mdx`}
+      >
+        the remote origin
+      </a>{' '}
+      to propose changes in a version controlled env.
+    </p>
+  </>
+)
 
 export default GithubFooter
