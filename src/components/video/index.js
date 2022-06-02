798f20a4bd9dcd1fec82080c7f6f3a0cf6f3ede9
gscottqueen <geoffreysqueen@gmail.com>
2022-06-02 15:55:52 -0400

    new video component

---
+++ src/components/video/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.css'
+
+const Video = ({ id, videoRef, ...props }) => (
+  <video id={id} playsInline autoPlay muted ref={videoRef} {...props} />
+)
+
+export default Video
