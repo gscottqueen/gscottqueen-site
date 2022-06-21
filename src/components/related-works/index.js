7c1b117bd674239e3ed30e0ea6d7ced0fac76d12
gscottqueen <geoffreysqueen@gmail.com>
2022-06-21 15:05:25 -0400

    new related works component

---
+++ src/components/related-works/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import './index.css'
+
+const RelatedWorks = ({ related }) => {
+  console.log(related)
+  return (
+    <>
+      <h3>Related Works</h3>
+      <ul className="related-works">
+        {related.map((item, i) => (
+          <div className="related-works-block" key={item.title + i}>
+            <Link to={item.path}>
+              <li>{item.title}</li>
+            </Link>
+            {item.experiment && <span className="tag">experiment</span>}
+          </div>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default RelatedWorks
