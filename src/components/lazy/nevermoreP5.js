9c3530a185366b1074fb0312dc943a393e7abd48
gscottqueen <geoffreysqueen@gmail.com>
2022-07-18 10:31:26 -0400

    trying to resolve the blockers and shields

--- src/components/lazy/nevermoreP5.js
+++ src/components/lazy/nevermoreP5.js
@@ -11,7 +11,7 @@ import RavensTrimmed from '../../video/ravens-trimmed.mp4'
 let RAVEN = ''
 
 const INFO = {}
-INFO.time_opened = new Date()
+INFO.time_opened = INFO.time_opened || new Date()
 INFO.previous_requests =
   history.length > 0
     ? `you are being tracked through your last ${history.length} request`
@@ -19,7 +19,7 @@ INFO.previous_requests =
 
 const API_KEY = 'd29394918b24428581709b373f7ddb29'
 
-let words,
+let words = [],
   typographicVideo,
   resdiv,
   poemdiv,
@@ -29,40 +29,41 @@ let words,
   ravensSoundVideo,
   count = 0
 
-const drawTypographicVideo = (p5) => {
+const drawTypographicVideo = (p5, w, vid) => {
+  console.log('words', w)
   p5.textFont('monospace')
   // set dimensions of our captured graphic
-  typographicVideo.image(ravensVideo, 0, -40, 210, 180)
+  vid.image(ravensVideo, 0, -40, 210, 180)
 
   // create a pre text response from the tokenized array
   // using these characters to
   let res = '<pre style="margin: 0;">'
-  if (words.length > 45) {
-    for (let i = 0; i < 60; i++) {
-      // rows
-      let line = ''
-      for (let j = 0; j < 185; j++) {
-        // cols
-        const x = typographicVideo.get(Math.round(j * 1.143), i * 2)
-        let f = 1 - x[0] / 1500
-        f = f * f // square factor to lighten up, because less bright characters
-        const v = Math.round(f * 40)
-        const index = Math.floor(Math.random(words[v].length))
-        const chr = words[v][index]
-        line += chr
-      }
-      res += line + '<br>'
+  for (let i = 0; i < 60; i++) {
+    // rows
+    let line = ''
+
+    for (let j = 0; j < 185; j++) {
+      // cols
+      const x = vid.get(Math.round(j * 1.143), i * 2)
+      let f = 1 - x[0] / 1500
+      f = f * f // square factor to lighten up, because less bright characters
+      const v = Math.round(f * 40)
+      const index = words[v] && Math.floor(Math.random(words[v].length))
+      const chr = words[v] && w[v][index]
+      line += chr
     }
+    res += line + '<br>'
   }
   res += '</pre>'
   resdiv.class('resdiv')
   resdiv.html(res)
 }
 
-const drawPoem = () => {
-  console
-  let stringFromPoem = words[count]
-  let poem = `<div style="
+const drawPoem = (c) => {
+  let stringFromPoem = words[c]
+  let poem = `<div
+    class="poem"
+    style="
     position: absolute;
     font-size: 60px;
     width: 100vw;
@@ -86,62 +87,38 @@ const toggleVid = () => {
 }
 
 const NevermoreP5 = () => {
-  const preload = () => {}
-
-  const setup = (p5) => {
+  const preload = () => {
     try {
-      fetch('https://api.ipify.org?format=json').then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText)
-        }
-      })
-    } catch (error) {
-      console.log(error)
-    }
-
-    try {
-      fetch('https://api.ipify.org?format=json')
+      fetch('http://ip-api.com/json')
         .then((response) => {
           if (response.ok) return response.json()
           else throw new Error('Status code error :' + response.status)
         })
         .then((data) => {
-          INFO.ip_address = data.ip
+          INFO.ip_address = data.query
+          INFO.country = data.country
+          INFO.timezone = data.timezone
+          INFO.region = data.regionName
+          INFO.city = data.city
+          INFO.zip = data.zip
+          INFO.latitude = data.lat
+          INFO.longitude = data.lon
+          INFO.internet_service_provider = data.isp
+          INFO.origin = data.org
+          INFO.hostname = data.query
           try {
-            fetch(`https://ip-api.com/json/${data.ip}`)
+            fetch(
+              `https://api.ipgeolocation.io/astronomy?apiKey=${API_KEY}&lat=${data.lat}&long=${data.lon}`
+            )
               .then((response) => {
                 if (response.ok) return response.json()
                 else throw new Error('Status code error :' + response.status)
               })
               .then((data) => {
-                INFO.country = data.country
-                INFO.timezone = data.timezone
-                INFO.region = data.regionName
-                INFO.city = data.city
-                INFO.zip = data.zip
-                INFO.latitude = data.lat
-                INFO.longitude = data.lon
-                INFO.internet_service_provider = data.isp
-                INFO.origin = data.org
-                INFO.hostname = data.query
-                try {
-                  fetch(
-                    `https://api.ipgeolocation.io/astronomy?apiKey=${API_KEY}&lat=${data.lat}&long=${data.lon}`
-                  )
-                    .then((response) => {
-                      if (response.ok) return response.json()
-                      else
-                        throw new Error('Status code error :' + response.status)
-                    })
-                    .then((data) => {
-                      INFO.length_of_day = data.day_length
-                      INFO.solar_noon = data.solar_noon
-                      INFO.current_distance_from_the_sun = data.sun_distance
-                      INFO.current_distance_from_the_moon = data.moon_distance
-                    })
-                } catch (error) {
-                  console.error('error', error)
-                }
+                INFO.length_of_day = data.day_length
+                INFO.solar_noon = data.solar_noon
+                INFO.current_distance_from_the_sun = data.sun_distance
+                INFO.current_distance_from_the_moon = data.moon_distance
               })
           } catch (error) {
             console.error('error', error)
@@ -150,7 +127,9 @@ const NevermoreP5 = () => {
     } catch (error) {
       console.error('error', error)
     }
+  }
 
+  const setup = (p5) => {
     // set up our canvas
     p5.createCanvas(p5.windowWidth, p5.windowHeight)
 
@@ -197,8 +176,7 @@ const NevermoreP5 = () => {
     w.setInterval(function () {
       if (playing === true) {
         resdiv.show()
-        words !== undefined && drawPoem()
-        console.log(words)
+        drawPoem(count)
         if (count > words.length) count = 0
         count++
       }
@@ -206,6 +184,12 @@ const NevermoreP5 = () => {
   }
 
   const draw = (p5) => {
+    if (INFO.length < 185) {
+      alert(
+        'This experiment requires anonymous calls to an open source api, please turn off your blockers and shields for the best experience'
+      )
+      return // once we have data draw will function as expected
+    }
     words = RiTa.tokenize(RAVEN.toUpperCase())
     // replace with space
     words.forEach((word, i) =>
@@ -220,7 +204,7 @@ const NevermoreP5 = () => {
       RAVEN = `${RAVEN} ${k} ${v} \n \n`
     }
 
-    drawTypographicVideo(p5)
+    drawTypographicVideo(p5, words, typographicVideo)
   }
 
   return (
--- src/experiments/nevermore.js
+++ src/experiments/nevermore.js
@@ -1,23 +1,11 @@
 import React from 'react'
 import { ExperimentDescription, BackNavLink, Layout, Seo } from '../components'
 import { w } from '../const'
-import { detectAnyAdblocker } from 'just-detect-adblock'
 // this drawing includes an audio aspect, sound on!!
 
 const NevermoreP5 = React.lazy(() => import('../components/lazy/nevermoreP5'))
 
 const Singulartiy = ({ pageContext }) => {
-  w &&
-    detectAnyAdblocker().then((detected) => {
-      console.log('working')
-      console.log(detected)
-      if (detected) {
-        console.log(detected)
-        alert(
-          'This experiment requires access to some open source api calls, please turn off your browser shields and adblockers.'
-        )
-      }
-    })
   const { data } = pageContext
   return (
     <Layout nonav>
