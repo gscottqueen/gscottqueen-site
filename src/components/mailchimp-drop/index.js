e988f4d97c88acf5e569089e64f3fd7beeffb6e2
gscottqueen <geoffreysqueen@gmail.com>
2022-11-15 15:00:14 -0500

    updated action

--- src/components/mailchimp-drop/index.js
+++ src/components/mailchimp-drop/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Mailchimp from 'react-mailchimp-form'
 
+import './index.css'
+
 const MailChimpDrop = ({ cta }) => {
   return (
-    <>
+    <div className="mailchimp-drop-wrapper">
       <h2>{cta ? cta : 'Get the drop!'}</h2>
       <Mailchimp
-        ction="https://gscottqueen.us14.list-manage.com/subscribe/post?u=036f380089a46d5d0d691b193&amp;id=a075acbe01&amp;f_id=009dafe0f0"
+        action="https://gscottqueen.us14.list-manage.com/subscribe/post?u=036f380089a46d5d0d691b193&amp;id=a075acbe01&amp;f_id=009dafe0f0"
+        className="mailchimp-drop-form"
         fields={[
           {
             name: 'EMAIL',
@@ -16,7 +19,7 @@ const MailChimpDrop = ({ cta }) => {
           }
         ]}
       />
-    </>
+    </div>
   )
 }
 
