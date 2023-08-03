9dd5d02e121089a76d94a3d9959eee59528eabb7
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-08-03 09:58:46 -0400

    updated css and title for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import { Layout, Seo, RegenButton } from '../components'
+import './index.css'
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -16,6 +17,18 @@ const IndexPage = () => {
   return (
     <Layout bgImage={data?.placeholderImage}>
       <Seo slug={'/'} />
+      <div className="home-title">
+        <h2 className="embossing-tape">
+          <span>
+            <span>INTEGRATING HUMAN DATA IN VIRTUAL ENVIRONMENTS</span>
+          </span>
+        </h2>
+        <div className="embossing-tape">
+          <span>
+            <span>New Works Summer 2023</span>
+          </span>
+        </div>
+      </div>
       <RegenButton anchor />
     </Layout>
   )
